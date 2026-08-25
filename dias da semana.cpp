f34bfc91fc358e607d499ba350ca1685dabcebac
Leticia Caumo Botelho <letibot4@gmail.com>
2026-08-25 09:03:56 -0300

    gerar dias da semana em c++
    25/08

---
+++ dias da semana.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int op;
+
+int main(){
+	
+	printf("digite o numero!!");
+	scanf("%d", &op);
+	
+	switch(op){
+		
+    case 1:
+	printf("domingo");
+	break;
+	
+    case 2:
+	printf("segunda-feira");
+	break;
+	
+	case 3:
+	printf("terca-feira");
+	break;
+	
+	case 4:
+	printf("quarta-feira");
+	break;
+	
+	case 5:
+	printf("quinta-feira");
+	break;
+	
+	case 6:
+	printf("sexta-feira");
+	break;
+	
+	case 7:
+	printf("sabado");
+	break;
+	
+	
+	return 0;
+	
+	
+	
+	
+			 			  
+	
+	
+	}
+	
+	
+
+
+
+
+}
+
