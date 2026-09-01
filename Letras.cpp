88b283a9347ef84a36c89ddf4c66c0b058b1e3e0
Leticia <letibot4@gmail.com>
2026-09-01 08:34:04 -0300

    Identificação de letras
    Atividade C++: identificação de letras feita em sala de aula 01/09

---
+++ Letras.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+char letra;
+
+int main(){
+
+ printf("digite uma letra: "); 
+ scanf("%c", &letra );
+
+ printf("a letra que vc digitou foi: %c", letra);
+
+
+
+
+ return 0;
+ 
+}
+
+
